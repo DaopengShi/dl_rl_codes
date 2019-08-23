dd8f46622c54b4d2e9cb7e4894e4e67c01634568
DaopengShi <holmessherlock734@gmail.com>
2019-08-23 23:23:26 +0800

    add ignite

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import torch
+import torchvision
+from torchvision import transforms, datasets, models
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import Accuracy, Loss
+import torch.optim as optim
 
 print(torch.__version__)
