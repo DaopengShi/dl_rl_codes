43e320d736d577993a43b4f58fa0c9e84b4c7c23
DaopengShi <holmessherlock734@gmail.com>
2019-08-31 14:50:32 +0800

    change path of data

--- vgg_tl.py
+++ vgg_tl.py
@@ -14,10 +14,10 @@ transform = transforms.Compose([
     transforms.Resize(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-trainset = datasets.ImageFolder(root='/home/holmessherlock734/data/Fruit-Images-Dataset/Training', transform=transform)
+trainset = datasets.ImageFolder(root='~/data/Fruit-Images-Dataset/Training', transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=150,
                                           shuffle=True, num_workers=4)
-testset = datasets.ImageFolder(root='/home/holmessherlock734/data/Fruit-Images-Dataset/Test', transform=transform)
+testset = datasets.ImageFolder(root='~/data/Fruit-Images-Dataset/Test', transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=150,
                                          shuffle=False, num_workers=4)
 
